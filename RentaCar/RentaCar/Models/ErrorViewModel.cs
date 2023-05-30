99f2794a01473c2ff8c1a7853540aeada07eef3d
okanalagas <okanalagas@hotmail.com>
2023-05-30 10:00:39 +0300

    ASP.NET Project

---
+++ RentaCar/RentaCar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentaCar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
